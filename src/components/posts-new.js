e9de0c017da8b3aac82309770eb50c8a5c5e099b
Chris F Carroll <chris.carroll@unforgettable.me.uk>
2017-09-15 10:49:18 +0100

    to end of section 9 lecture 139

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -1,13 +1,49 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
+import {Field, reduxForm} from 'redux-form'
+import _ from 'lodash'
 
-console.log('in posts-new.js');
+class PostsNew extends Component {
 
-class PostsNew extends Component{
+  onSubmit(values){
+    _.forOwn(values, (v,k)=>{console.log("submitted:", k,":",v)});
+  }
+
+  constructor(props){
+    super(props);
+    this.onSubmit=this.onSubmit.bind(this);
+  }
 
   render(){
-    return (<div>Boo</div>);
+
+    const {handleSubmit}=this.props;
+
+    return (<form onSubmit={handleSubmit(this.onSubmit)}>
+      <Field name="title" component={this.renderField} label="Post Title"/>
+      <Field name="categories" component={this.renderField}  label="Categories"/>
+      <Field name="content" component={this.renderField}  label="Content"/>
+      <button type="submit" className="btn btn-primary">Post</button>
+    </form>);
+  }
+
+  renderField(field){
+    return (<div className="form-group">
+              <label>{field.label}</label>
+              <input {...field.input} className="form-control"/>
+              <div className="alert-danger">{field.meta.touched?field.meta.error:''}</div>
+            </div>);
   }
 
 }
 
-export default PostsNew;
+const textboxFor= ({label, field})=> (<div><label>{label}</label><input {...field.input} /></div>);
+
+function validate(values){
+  const errors={};
+  _.mapKeys(['title','content'], (f)=>{ values[f]||(errors[f]= `Please enter ${f}.`) });
+
+  _.forOwn(values, (v,f)=>{ console.log("value: ",f,v)});
+  _.forOwn(errors, (v,f)=>{ console.log("error: ",f,v)});
+  return errors;
+}
+
+export default reduxForm({validate, form:'PostsNewForm'})(PostsNew);
